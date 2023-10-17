8d3851df6e576ffeadb259267888a4c350420cc3
Bhavya Patel <bhavya.neofruition@gmail.com>
2023-10-17 18:21:21 +0530

    TestSetvalue()

--- cache-demo/src/test/java/provider/RedisCacheManTest2.java
+++ cache-demo/src/test/java/provider/RedisCacheManTest2.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(classes = RedisConfiguration.class)
@@ -21,6 +25,9 @@ public class RedisCacheManTest2 {
 
     @Mock
     private RedisTemplate<String, String> redisTemplate;
+    @Mock
+    private RedisConnectionFactory redisConnectionFactory;
+
 
     @Mock
     private ValueOperations<String, String> valueOperations;
@@ -33,12 +40,18 @@ public class RedisCacheManTest2 {
     @Test
     public void testGetValue() throws Exception {
         String key = "testKey";
-        String expectedValue = "testValue";
-//        redisCacheMan.set(key, expectedValue);
-        when(redisTemplate.opsForValue().get(key)).thenReturn(key +", "+ expectedValue);
+        String value = "testValue";
+        when(redisTemplate.opsForValue().get(key)).thenReturn(value);
 
         String actualValue = redisCacheMan.get(key);
 
-        Assertions.assertEquals(key +", "+ expectedValue, actualValue);
+        Assertions.assertEquals(value, actualValue);
+    }
+    @Test
+    public void testSetValue() {
+        String key = "testKey";
+        String value = "testValue";
+        redisTemplate.opsForValue().set("key", "value");
+        verify(redisConnectionFactory.getConnection()).set(key.getBytes(), value.getBytes());
     }
 }
